79466fef781f5399da505fd76768e389a159a725
chauhan98 <cshashank.2598@gmail.com>
2021-10-16 15:44:28 +0530

    day 5 programs

---
+++ QuotientAndRemainder.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class QuotientAndRemainder {
+	public static void main(String[] args) {
+
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the number to be divided: ");
+
+		int dividend =sc.nextInt();
+		System.out.println("Enter your divisor value: ");
+
+
+		int divisor = sc.nextInt();
+
+	    int quotient = dividend / divisor;
+	    int remainder = dividend % divisor;
+
+	    System.out.println("Quotient = " + quotient);
+	    System.out.println("Remainder = " + remainder);
+	}
+
+}
